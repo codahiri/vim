eac715851cd8abba405436b2b3e8720bf1c2a956
ilosrim <ilosrim@yahoo.com>
2021-12-14 02:01:30 +0500

    bash file

---
+++ a.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "Welcome to a 2.1"
+echo "Press Ctrl+C to exit"
+
+while true
+do
+	i=$((i+1))
+	
+	if [ $i -gt 5000 ]
+		then
+			git push origin
+			i=0
+	fi
+	
+	echo "a" >> a
+	
+	git add . > /dev/null
+	git commit -m "a" > /dev/null
+	
+	rm a
+	
+	git add . > /dev/null
+	git commit -m "a" > /dev/null
+done
